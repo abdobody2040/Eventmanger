cfa77eb93e79ebbcbb2efe11330e8b657bfdd9e1
MazenHesham-coder <hmazen567@gmial.com>
2025-08-07 13:58:24 +0200

    Updating the main.py

--- app.py
+++ app.py
@@ -1863,11 +1863,3 @@ with app.app_context():
 
     db.session.commit()
 
-if __name__ == '__main__':
-    # Get configuration from .env
-    host = os.environ.get('FLASK_HOST', '0.0.0.0')
-    port = int(os.environ.get('FLASK_PORT', '5000'))
-    debug = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
-
-    print(f"Starting Flask server on {host}:{port} (debug={debug})")
-    app.run(host=host, port=port, debug=debug)
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from app import app
-
+import os
 if __name__ == '__main__':
-    app.run()
+    
+    # Get configuration from .env
+    host = os.environ.get('FLASK_HOST', '0.0.0.0')
+    port = int(os.environ.get('FLASK_PORT', '4000'))
+    debug = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
+
+    print(f"Starting Flask server on {host}:{port} (debug={debug})")
+    app.run(host=host, port=port, debug=debug)
